6319d65d53efb2acfb10b4e355adfd738600d9ce
usc3l <lishunli.me@gmail.com>
2024-10-16 14:53:09 +0800

    use lastet release link

--- sb_00.sh
+++ sb_00.sh
@@ -140,7 +140,7 @@ download_singbox() {
   if [ "$ARCH" == "arm" ] || [ "$ARCH" == "arm64" ] || [ "$ARCH" == "aarch64" ]; then
       FILE_INFO=("https://github.com/eooce/test/releases/download/arm64/sb web" "https://github.com/eooce/test/releases/download/arm64/bot13 bot" "https://github.com/eooce/test/releases/download/ARM/swith npm")
   elif [ "$ARCH" == "amd64" ] || [ "$ARCH" == "x86_64" ] || [ "$ARCH" == "x86" ]; then
-      FILE_INFO=("https://github.com/usc/build-sing-box/releases/download/2024101007/sing-box-freebsd-amd64-v3 web" "https://github.com/eooce/test/releases/download/freebsd/server bot" "https://github.com/eooce/test/releases/download/freebsd/npm npm")
+      FILE_INFO=("https://github.com/usc/build-sing-box/releases/latest/download/sing-box-freebsd-amd64-v3 web" "https://github.com/eooce/test/releases/download/freebsd/server bot" "https://github.com/eooce/test/releases/download/freebsd/npm npm")
   else
       echo "Unsupported architecture: $ARCH"
       exit 1
